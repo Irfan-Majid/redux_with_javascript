7cee3d3dba7362271121f6ad6ba18145306e6442
Abdul Majid Irfan <78084618+Irfan-Majid@users.noreply.github.com>
2023-01-16 23:59:36 +0600

    redux_logger

--- index.js
+++ index.js
@@ -1,19 +1,14 @@
-const  {createStore, combineReducers} = require('redux');
+const  {createStore, combineReducers, applyMiddleware} = require('redux');
+const { default: logger } = require('redux-logger');
 
 const ADD_PRODUCT = 'ADD_PRODUCT'
 const GET_PRODUCT = 'GET_PRODUCT'
-const ADD_CART = 'ADD_CART'
-const GET_CART = 'GET_CART'
 
 const ProductInitialState = {
     products: ['suger','salt'],
     count : 2
 }
 
-const cartInitialState = {
-    products: ['suger'],
-    count : 1
-}
 
 const addProduct = (product) => {
     return {
@@ -28,18 +23,7 @@ const getProduct = () => {
     };
 }
 
-const addCart = (product) => {
-    return {
-        type : ADD_CART,
-        payload : product,
-    };
-}
 
-const getCart = () => {
-    return {
-        type : GET_CART
-    };
-}
 
 
 
@@ -60,45 +44,12 @@ const productReducer = (state=ProductInitialState,action) => {
 }
 
 
-const cartReducer = (state=cartInitialState,action) => {
-    switch (action.type){
-        case ADD_CART:
-            return {
-                products:[...state.products,action.payload],
-                count:state.count + 1,
-            }
-        case GET_CART:
-            return {
-                ...state
-            }
-        default:
-            return state;
-    }
-}
-
-
-
-const rootReducer = combineReducers({
-    productR: productReducer,
-    cartR: cartReducer
-});
-
-const store = createStore(rootReducer);
+const store = createStore(productReducer,applyMiddleware(logger));
 
 store.subscribe(()=>{
     console.log(store.getState());
 })
 
-store.dispatch(getCart());
-store.dispatch(addCart('irf'));
-store.dispatch(getCart());
-
 store.dispatch(getProduct());
 store.dispatch(addProduct('irfa '));
 store.dispatch(getProduct());
-// store.dispatch(incrementAction());
-// store.dispatch(decrementAction());
-// store.dispatch(resetAction());
-// store.dispatch(incrementAction());
-// store.dispatch(incrementAction());
-// store.dispatch(incrementByValue(5));
